2e4087989f63dc589e4a508ee1b6eabfabf4306f
JohnAndrewFonseca <johnandrewfonseca@gmail.com>
2025-12-11 14:20:18 +0800

    Tab Title Update

--- resources/views/components/dashboard-layout.blade.php
+++ resources/views/components/dashboard-layout.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>dashboard</title>
+    <title>NutriTrack | Dashboard</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @vite(['resources/css/dashboard.css', 'resources/js/dashboard.js'])
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>NutriTrack | Login</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
